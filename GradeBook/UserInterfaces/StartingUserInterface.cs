4bfe7f1cec971c4603d51a7e5a200587f8b34293
Alex <awarrington0895@gmail.com>
2018-02-14 22:54:55 -0600

    Hotfixing format errors in UserInterface

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,7 +51,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine(type + "is not a supported type of gradebook, please try again");
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
@@ -79,8 +79,7 @@ namespace GradeBook.UserInterfaces
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook" +
-                ". and 'Type' is what type of grading it should use");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
